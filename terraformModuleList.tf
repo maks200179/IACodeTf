bd3cf8a586489c61e91501d123eb52290345b779
Max <maks200179@mail.ru>
2020-03-28 23:41:00 +0300

    Update terraformModuleList.tf

--- terraformModuleList.tf
+++ terraformModuleList.tf
@@ -2,13 +2,13 @@
   
   
 module "aws_kubernetes_test_network_terraform_conf" {
-  source = "../moduls/kubernetes/aws_kubernetes_test_network_terraform_conf"
+  source = "../moduls/iacode/aws_kubernetes_test_network_terraform_conf"
 } 
   
  module "docker_compose_env_kubernetes_test_server_kubernetes_manager" {
-  source = "../moduls/kubernetes/docker_compose_env_kubernetes_test_server_kubernetes_manager"
+  source = "../moduls/iacode/docker_compose_env_kubernetes_test_server_kubernetes_manager"
 }  
 
  module "docker_compose_env_kubernetes_test_server_kubernetes_worker1" {
-  source = "../moduls/kubernetes/docker_compose_env_kubernetes_test_server_kubernetes_worker1"
+  source = "../moduls/iacode/docker_compose_env_kubernetes_test_server_kubernetes_worker1"
 }  
