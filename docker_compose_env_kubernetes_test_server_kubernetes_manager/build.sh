f3d98ee82f2fdded21059659cf65a670f6468232
Max <maks200179@mail.ru>
2020-03-29 14:44:44 +0300

    Update build.sh

--- docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
+++ docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
@@ -48,7 +48,27 @@ done
 
         fi
         
-        
+        sudo mkdir /etc/docker
+        # Setup daemon.
+        cat > /etc/docker/daemon.json <<EOF
+        {
+          "exec-opts": ["native.cgroupdriver=systemd"],
+          "log-driver": "json-file",
+          "log-opts": {
+            "max-size": "100m"
+          },
+          "storage-driver": "overlay2",
+          "storage-opts": [
+            "overlay2.override_kernel_check=true"
+          ]
+        }
+EOF
+
+        mkdir -p /etc/systemd/system/docker.service.d
+
+        # Restart Docker
+        systemctl daemon-reload
+        systemctl restart docker
      
         
 
