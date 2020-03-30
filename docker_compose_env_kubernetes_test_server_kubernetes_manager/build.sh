9f703f103ad2eeab3dd512c2a10f5553eaa2d4a8
Max <maks200179@mail.ru>
2020-03-30 11:58:48 +0300

    Update build.sh

--- docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
+++ docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
@@ -156,7 +156,7 @@ EOF
             #for root 
             #sudo export KUBECONFIG=/etc/kubernetes/admin.conf
             
-            
+            sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
             #sudo kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
                 
         else 
