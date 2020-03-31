01aba751a0041634e6f662dcce23ccfe111dcce8
Max <maks200179@mail.ru>
2020-03-31 15:56:32 +0300

    Update build.sh

--- docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
+++ docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
@@ -141,6 +141,7 @@ EOF
         if  [[ ! -z $(yum list installed | grep docker-ce.x86_64) ]] && [[ ! -z $(docker-compose --version) ]] ; then
                  #sudo whoami
                  #whoami
+                 sudo -s
                  sudo bash -c "kubectl cluster-info  | egrep --color  'Kubernetes master'"
                 
         fi
