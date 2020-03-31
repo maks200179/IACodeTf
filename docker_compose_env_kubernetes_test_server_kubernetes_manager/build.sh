1521f225e132ec6484433dfda887e0141c8d838e
Max <maks200179@mail.ru>
2020-03-31 18:04:47 +0300

    Update build.sh

--- docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
+++ docker_compose_env_kubernetes_test_server_kubernetes_manager/build.sh
@@ -139,8 +139,10 @@ EOF
 
     if  [[ $check_cluster_up == "yes" ]] ; then
         if  [[ ! -z $(yum list installed | grep docker-ce.x86_64) ]] && [[ ! -z $(docker-compose --version) ]] ; then
-                 #sudo whoami
-                 #whoami
+                 sudo whoami
+                 whoami
+                 echo $KUBECONFIG
+                 echo $HOME
                  sudo -s
                  sudo kubectl cluster-info  | egrep --color  'Kubernetes master' | sed 's/\x1b\[[0-9;]*m//g'
                 
