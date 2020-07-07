0614ee1be3f061da754187060be1c5d9a12ba5c1
Max <maks200179@mail.ru>
2020-07-08 00:20:54 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -62,7 +62,7 @@ module "vpc" {
   
 module "my-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
+  cluster_name    = local.cluster_name
   cluster_version = "1.16"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id  
