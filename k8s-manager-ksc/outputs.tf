b960e2cc619e409bf995dd93f92f86f5e98e7217
Max <maks200179@mail.ru>
2020-07-08 22:55:36 +0300

    Update outputs.tf

--- k8s-manager-ksc/outputs.tf
+++ k8s-manager-ksc/outputs.tf
@@ -3,14 +3,20 @@ data "template_file" "json_config" {
     
     template = <<EOF
 {
-    "endpoint": "${module.eks.cluster_endpoint}",
-    "securitygroup": "${module.eks.cluster_security_group_id}",
-    "kubectl_config": "${module.eks.kubeconfig}",
-    "config_map_aws_auth": "${module.eks.config_map_aws_auth}"
+    "endpoint": "${module.my-cluster.cluster_endpoint}",
+    "securitygroup": "${module.my-cluster.cluster_security_group_id}",
+    "kubectl_config": "${module.my-cluster.kubeconfig}",
+    "config_map_aws_auth": "${module.my-cluster.config_map_aws_auth}"
 }
 }
 EOF
 }
 
 
+
+resource "local_file" "environment1" {
+    content = "endpoint": "${module.my-cluster.cluster_endpoint}"
+    filename = "../terraform/modules_data/k8s-manager-ksc/json.info"
+    file_permission = "0400"
+}  
 
