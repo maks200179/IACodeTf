5e40cd31d0caca2571e5edaac8e083adec028212
Max <maks200179@mail.ru>
2020-07-08 13:52:46 +0300

    Update outputs.tf

--- k8s-manager-ksc/outputs.tf
+++ k8s-manager-ksc/outputs.tf
@@ -1,1 +1,16 @@
 
+data "template_file" "json_config" {
+    
+    template = <<EOF
+{
+    "endpoint": "${module.eks.cluster_endpoint}",
+    "securitygroup": "${module.eks.cluster_security_group_id}",
+    "kubectl_config": "${module.eks.kubeconfig}",
+    "config_map_aws_auth": "${module.eks.config_map_aws_auth}"
+}
+}
+EOF
+}
+
+
+
