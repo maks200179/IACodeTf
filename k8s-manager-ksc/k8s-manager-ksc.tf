10a3e21220121a7e8113bb6135bc240ec3a92cb2
Max <maks200179@mail.ru>
2020-07-18 02:33:38 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -49,6 +49,10 @@ data "aws_eks_cluster" "cluster" {
   name = module.my-cluster.cluster_id
 }
 
+  
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}  
 
 data "aws_availability_zones" "available" {
 }
