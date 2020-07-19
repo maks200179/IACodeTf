2981cdafcf41e4e7732c65c55ce3a95be90bdb35
Max <maks200179@mail.ru>
2020-07-19 18:29:27 +0300

    Update module.tf

--- ecs_example_ecs-prod/module.tf
+++ ecs_example_ecs-prod/module.tf
@@ -1,66 +1,87 @@
-## This Template creates the infrastructure
-## with bastion host
-##
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 1.26.0"
 
-module "main-vpc" {
-  source     = "../../modules/vpc"
-  ENV        = "${var.ENV}"                    #productEnv
-  AWS_REGION = "${var.AWS_REGION}"
-  VPC_NAME   = "${var.VPC_NAME}"               #productID
-  az1        = "${var.AWS_REGION}a"
-  az2        = "${var.AWS_REGION}b"
+  name               = "app-dev"
+  cidr               = "10.10.10.0/24"
+  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets    = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/27"]
+  public_subnets     = ["10.10.10.96/27", "10.10.10.128/27", "10.10.10.160/27"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = {
+    Environment = "dev"
+    Owner = "me"
+  }
 }
 
-module "bastion" {
-  source         = "../../modules/bastion"
-  ENV            = "${var.ENV}"
-  AWS_REGION     = "${var.AWS_REGION}"
-  VPC_NAME       = "${var.VPC_NAME}"                     #productID
-  vpc_id         = "${module.main-vpc.vpc_id}"           #productVPC
-  public_subnets = "${module.main-vpc.public_subnets-1}"
-  keyname        = "bastion-key"                         #Key name
-  pubkey         = "ssh-rsa xxxxxxxxxxxxxxxxxxxxxxx"     #public key
+module "ecs_cluster" {
+  source = "anrim/ecs/aws//modules/cluster"
+
+  name = "app-dev"
+  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_subnets = ["${module.vpc.private_subnets}"]
+  tags        = {
+    Environment = "dev"
+    Owner = "me"
+  }
 }
-## This templates creates the Application Loadblancer and 
-## setup the ECS Cluster
-##
 
 module "alb" {
-  source               = "../../modules/loadblancer/application_loadblancer"
-  ENV                  = "dev" # Product Environment
-  VPC_NAME             = "fusion" # Product Name
-  appname              = "ecs-demo" # Application Name
-  vpc_id               = "${module.main-vpc.vpc_id}"
-  public-subnet-1      = "${module.main-vpc.public_subnets-1}"
-  public-subnet-2      = "${module.main-vpc.public_subnets-2}"
-  certificate_arn      = "xxxxxxxxxxxxxxxxxxxxxxxx"
+  source = "anrim/ecs/aws//modules/alb"
+
+  name            = "app-dev"
+  host_name       = "app"
+  domain_name     = "example.com"
+  certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+  backend_sg_id   = "${module.ecs_cluster.instance_sg_id}"
+  tags            = {
+    Environment = "dev"
+    Owner = "me"
+  }
+  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_subnets = ["${module.vpc.public_subnets}"]
 }
 
-module "ecs" {
-  source               = "../../modules/ecs"
-  ENV                  = "prod" # Product Environment
-  VPC_NAME             = "fusion" # Product Name
-  vpc_id               = "${module.main-vpc.vpc_id}"
-  public-subnet-1      = "${module.main-vpc.public_subnets-1}"
-  public-subnet-2      = "${module.main-vpc.public_subnets-2}"
-  appname              = "ecs-demo" # Application Name
-  min_size             = "1" # default 1, Minimum no of Instance node (ASG)
-  max_size             = "4" # default 4, Maximum no of Instance node (ASG)
-  desired_capacity     = "2" # default 2, number of Amazon EC2 instances that should be running
-  keyname              = "ecskey"# key Name
-  pubkey               = "ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # Public key
-  instance_type        = "t2.micro" # default t2.micro
-  iam_instance_profile = "${module.IAM.instance_profile}"
-  applicationPort      = "80" # Application port no (for security group)
-  iam_role             = "${module.IAM.iam_role}"
-  target_group         = "${module.alb.tagetGroup}"
-  container_name       = "demo-ecs-container" # Container Name
+resource "aws_ecs_task_definition" "app" {
+  family = "app-dev"
+  container_definitions = <<EOF
+[
+  {
+    "name": "nginx",
+    "image": "nginx:1.13-alpine",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "app-dev-nginx",
+        "awslogs-region": "us-east-1"
+      }
+    },
+    "memory": 128,
+    "cpu": 100
+  }
+]
+EOF
+}
+
+module "ecs_service_app" {
+  source = "anrim/ecs/aws//modules/service"
+
+  name = "app-dev"
+  alb_target_group_arn = "${module.alb.target_group_arn}"
+  cluster              = "${module.ecs_cluster.cluster_id}"
+  container_name       = "nginx"
   container_port       = "80"
-  ecs_task_family      = "nginx" # Task defination name
-  container_definitions= "${file("container_definition.json")}"
-  desired_count        = "2" # default 1
-  launch_type          = "EC2" # valid values are EC2 and FARGATE, default is EC2
-  iam_policy_name      = "ecs-policy"     #Policy Name
-  iam_instance_profile = "ecs-profile"    # Instance profile Name
-  iam_role             = "fusion-prod-ecs" # Role Name
+  log_groups           = ["app-dev-nginx"]
+  task_definition_arn  = "${aws_ecs_task_definition.app.arn}"
+  tags                 = {
+    Environment = "dev"
+    Owner = "me"
+  }
 }
