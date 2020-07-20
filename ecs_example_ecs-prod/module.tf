564ff4c8e37e8820d2c31d7d9b676e4d91eaa148
Max <maks200179@mail.ru>
2020-07-20 17:37:50 +0300

    Update module.tf

--- ecs_example_ecs-prod/module.tf
+++ ecs_example_ecs-prod/module.tf
@@ -1,87 +1,142 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 1.26.0"
-
-  name               = "app-dev"
-  cidr               = "10.10.10.0/24"
-  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets    = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/27"]
-  public_subnets     = ["10.10.10.96/27", "10.10.10.128/27", "10.10.10.160/27"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  tags               = {
-    Environment = "dev"
-    Owner = "me"
-  }
+resource "aws_key_pair" "alex" {
+    key_name = "alex-key"
+    public_key = "${file(var.ssh_pubkey_file)}"
 }
 
-module "ecs_cluster" {
-  source = "anrim/ecs/aws//modules/alb"
-
-  name = "app-dev"
-  vpc_id      = "${module.vpc.vpc_id}"
-  vpc_subnets = ["${module.vpc.private_subnets}"]
-  tags        = {
-    Environment = "dev"
-    Owner = "me"
-  }
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
 }
 
-module "alb" {
-  source = "anrim/ecs/aws//modules/alb"
-
-  name            = "app-dev"
-  host_name       = "app"
-  domain_name     = "example.com"
-  certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-  backend_sg_id   = "${module.ecs_cluster.instance_sg_id}"
-  tags            = {
-    Environment = "dev"
-    Owner = "me"
-  }
-  vpc_id      = "${module.vpc.vpc_id}"
-  vpc_subnets = ["${module.vpc.public_subnets}"]
+resource "aws_route_table" "external" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main.id}"
+    }
 }
 
-resource "aws_ecs_task_definition" "app" {
-  family = "app-dev"
-  container_definitions = <<EOF
-[
-  {
-    "name": "nginx",
-    "image": "nginx:1.13-alpine",
-    "essential": true,
-    "portMappings": [
-      {
-        "containerPort": 80
-      }
-    ],
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "options": {
-        "awslogs-group": "app-dev-nginx",
-        "awslogs-region": "us-east-1"
-      }
-    },
-    "memory": 128,
-    "cpu": 100
-  }
-]
-EOF
+resource "aws_route_table_association" "external-main" {
+    subnet_id = "${aws_subnet.main.id}"
+    route_table_id = "${aws_route_table.external.id}"
 }
 
-module "ecs_service_app" {
-  source = "anrim/ecs/aws//modules/alb"
+# TODO: figure out how to support creating multiple subnets, one for each
+# availability zone.
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "${var.availability_zone}"
+}
 
-  name = "app-dev"
-  alb_target_group_arn = "${module.alb.target_group_arn}"
-  cluster              = "${module.ecs_cluster.cluster_id}"
-  container_name       = "nginx"
-  container_port       = "80"
-  log_groups           = ["app-dev-nginx"]
-  task_definition_arn  = "${aws_ecs_task_definition.app.arn}"
-  tags                 = {
-    Environment = "dev"
-    Owner = "me"
-  }
+resource "aws_internet_gateway" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_security_group" "load_balancers" {
+    name = "load_balancers"
+    description = "Allows all traffic"
+    vpc_id = "${aws_vpc.main.id}"
+
+    # TODO: do we need to allow ingress besides TCP 80 and 443?
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # TODO: this probably only needs egress to the ECS security group.
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "ecs" {
+    name = "ecs"
+    description = "Allows all traffic"
+    vpc_id = "${aws_vpc.main.id}"
+
+    # TODO: remove this and replace with a bastion host for SSHing into
+    # individual machines.
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = ["${aws_security_group.load_balancers.id}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_ecs_cluster" "main" {
+    name = "${var.ecs_cluster_name}"
+}
+
+resource "aws_autoscaling_group" "ecs-cluster" {
+    availability_zones = ["${var.availability_zone}"]
+    name = "ECS ${var.ecs_cluster_name}"
+    min_size = "${var.autoscale_min}"
+    max_size = "${var.autoscale_max}"
+    desired_capacity = "${var.autoscale_desired}"
+    health_check_type = "EC2"
+    launch_configuration = "${aws_launch_configuration.ecs.name}"
+    vpc_zone_identifier = ["${aws_subnet.main.id}"]
+}
+
+resource "aws_launch_configuration" "ecs" {
+    name = "ECS ${var.ecs_cluster_name}"
+    image_id = "${lookup(var.amis, var.region)}"
+    instance_type = "${var.instance_type}"
+    security_groups = ["${aws_security_group.ecs.id}"]
+    iam_instance_profile = "${aws_iam_instance_profile.ecs.name}"
+    # TODO: is there a good way to make the key configurable sanely?
+    key_name = "${aws_key_pair.alex.key_name}"
+    associate_public_ip_address = true
+    user_data = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}' > /etc/ecs/ecs.config"
+}
+
+
+resource "aws_iam_role" "ecs_host_role" {
+    name = "ecs_host_role"
+    assume_role_policy = "${file("policies/ecs-role.json")}"
+}
+
+resource "aws_iam_role_policy" "ecs_instance_role_policy" {
+    name = "ecs_instance_role_policy"
+    policy = "${file("policies/ecs-instance-role-policy.json")}"
+    role = "${aws_iam_role.ecs_host_role.id}"
+}
+
+resource "aws_iam_role" "ecs_service_role" {
+    name = "ecs_service_role"
+    assume_role_policy = "${file("policies/ecs-role.json")}"
+}
+
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
+    name = "ecs_service_role_policy"
+    policy = "${file("policies/ecs-service-role-policy.json")}"
+    role = "${aws_iam_role.ecs_service_role.id}"
+}
+
+resource "aws_iam_instance_profile" "ecs" {
+    name = "ecs-instance-profile"
+    path = "/"
+    roles = ["${aws_iam_role.ecs_host_role.name}"]
 }
