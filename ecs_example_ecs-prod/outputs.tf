1ac731162b04056da106d2889fbe0e909973bd5b
Max <maks200179@mail.ru>
2020-07-20 22:06:22 +0300

    Update outputs.tf

--- ecs_example_ecs-prod/outputs.tf
+++ ecs_example_ecs-prod/outputs.tf
@@ -1,19 +1,1 @@
-data "template_file" "json_config" {
-    
-    template = <<EOF
-{
-    "endpoint": "${aws_ecs_cluster.main.cluster_endpoint}",
-    "securitygroup": "${aws_ecs_cluster.main.cluster_security_group_id}",
-    "kubectl_config": "${aws_ecs_cluster.main.kubeconfig}"
-    
-}
-EOF
-}
 
-
-
-resource "local_file" "environment1" {
-    content = "${data.template_file.json_config.rendered}"
-    filename = "../terraform/modules_data/ecs_example_ecs-prod/json.secret"
-    file_permission = "0400"
-}  
