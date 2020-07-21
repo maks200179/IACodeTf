c543866808114d2439f255290f4e06e6954344e7
Max <maks200179@mail.ru>
2020-07-21 12:30:26 +0300

    Update module.tf

--- ecs_example_ecs-prod/module.tf
+++ ecs_example_ecs-prod/module.tf
@@ -112,17 +112,7 @@ resource "aws_autoscaling_group" "ecs-cluster" {
     vpc_zone_identifier = ["${aws_subnet.main.id}"]
 }
 
-resource "aws_launch_configuration" "ecs" {
-    name = "ECS ${var.ecs_cluster_name}"
-    image_id = "${lookup(var.amis, var.region)}"
-    instance_type = "${var.instance_type}"
-    security_groups = ["${aws_security_group.ecs.id}"]
-    iam_instance_profile = "${aws_iam_instance_profile.ecs.name}"
-    # TODO: is there a good way to make the key configurable sanely?
-    key_name = "${local.key_name}"
-    associate_public_ip_address = true
-    user_data = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}' > /etc/ecs/ecs.config"
-}
+
 
 
 resource "aws_iam_role" "ecs_host_role" {
@@ -151,4 +141,17 @@ resource "aws_iam_instance_profile" "ecs" {
     name = "ecs-instance-profile"
     path = "/"
     roles = ["${aws_iam_role.ecs_host_role.name}"]
+}
+
+
+resource "aws_launch_configuration" "ecs" {
+    name = "ECS ${var.ecs_cluster_name}"
+    image_id = "${lookup(var.amis, var.region)}"
+    instance_type = "${var.instance_type}"
+    security_groups = ["${aws_security_group.ecs.id}"]
+    iam_instance_profile = "${aws_iam_instance_profile.ecs.name}"
+    # TODO: is there a good way to make the key configurable sanely?
+    key_name = "${local.key_name}"
+    associate_public_ip_address = true
+    user_data = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}' > /etc/ecs/ecs.config"
 }
