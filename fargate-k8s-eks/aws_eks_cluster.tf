b544d2c508122ff4b198f82f4a2015792f71515e
Max <maks200179@mail.ru>
2020-08-06 00:33:12 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -23,6 +23,28 @@ output "kubeconfig-certificate-authority-data" {
 }
 
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "example" {
+  count = 2
+
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.example.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.example.id
+
+  tags = {
+    "kubernetes.io/cluster/${aws_eks_cluster.example.name}" = "shared"
+  }
+}
+
+
+
+
+
+
+
 
 resource "aws_iam_role" "example" {
   name = "eks-cluster-example"
