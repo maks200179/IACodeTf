38820319aefd9d5f11fd64fe53def5fbd47fbd77
Max <maks200179@mail.ru>
2020-08-06 17:14:33 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -30,13 +30,13 @@ resource "aws_subnet" "example" {
 }
 
 // create a dedicated subnet
-resource "aws_subnet" "example" {
-  vpc_id            = "${aws_vpc.example.id}"
+resource "aws_subnet" "example1" {
+  vpc_id            = "${aws_vpc.example1.id}"
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-2b"
  
   tags = {
-    Name = "kubernetes_test-subnet"
+    Name = "kubernetes_test-subnet2"
   }
 }
 
