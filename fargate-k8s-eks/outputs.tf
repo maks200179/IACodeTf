6cd77c3b913212248931b04d7a13033bede1a16f
Max <maks200179@mail.ru>
2020-08-06 21:42:00 +0300

    Update outputs.tf

--- fargate-k8s-eks/outputs.tf
+++ fargate-k8s-eks/outputs.tf
@@ -2,8 +2,8 @@ data "template_file" "json_config" {
     
     template = <<EOF
 {
-    "endpoint": "${aws_eks_cluster.example.cluster_endpoint}",
-    "securitygroup": "${aws_eks_cluster.example.cluster_security_group_id}",
+    "endpoint": "${aws_eks_cluster.example.endpoint}",
+    "securitygroup": "${aws_eks_cluster.example.security_group_id}",
     "kubectl_config": "${aws_eks_cluster.example.kubeconfig}"
     
 }
