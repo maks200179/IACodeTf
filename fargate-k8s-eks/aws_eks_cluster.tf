93046c5717542ac850fa4e74d274ec1dcb819274
Max <maks200179@mail.ru>
2020-08-06 18:31:19 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -46,7 +46,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.example[*].id
+    subnet_ids = [aws_subnet.example1.id,aws_subnet.example2.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
