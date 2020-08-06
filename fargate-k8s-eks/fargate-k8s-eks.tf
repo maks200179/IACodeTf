1ad9e37669295ba1cc2a0cb87360bc507ac97da8
Max <maks200179@mail.ru>
2020-08-06 18:33:55 +0300

    Update fargate-k8s-eks.tf

--- fargate-k8s-eks/fargate-k8s-eks.tf
+++ fargate-k8s-eks/fargate-k8s-eks.tf
@@ -6,7 +6,7 @@ resource "aws_eks_fargate_profile" "example" {
   cluster_name           = aws_eks_cluster.example.name
   fargate_profile_name   = "example"
   pod_execution_role_arn = aws_iam_role.example.arn
-  subnet_ids             = aws_subnet.example[*].id
+  subnet_ids             = [aws_subnet.example1.id,aws_subnet.example2.id]
 
   selector {
     namespace = "example"
