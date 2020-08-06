b745af8a16b9aeaa07d7b1790f464f018ff7e8e1
Max <maks200179@mail.ru>
2020-08-06 21:27:05 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "example1" {
   availability_zone = "us-east-2a"
  
   tags = {
-    "kubernetes.io/cluster/example1" = "shared"
+    "kubernetes.io/cluster/example" = "shared"
   }
 }
 
@@ -36,7 +36,7 @@ resource "aws_subnet" "example2" {
   availability_zone = "us-east-2c"
  
   tags = {
-    "kubernetes.io/cluster/example2" = "shared"
+    "kubernetes.io/cluster/example" = "shared"
   }
 }
 
