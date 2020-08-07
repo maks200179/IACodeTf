515bf1d39e964b0233aeb79699b7bfb726c4e252
Max <maks200179@mail.ru>
2020-08-07 23:50:12 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -41,34 +41,6 @@ resource "aws_subnet" "example2" {
 }
 
 
-resource "aws_eks_cluster" "example" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.example1.id,aws_subnet.example2.id]
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSServicePolicy,
-  ]
-}
-
-output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
-}
-
-
-
-
-
 
 
 
@@ -106,57 +78,43 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
 
 
 
-resource "aws_iam_role" "node-example" {
-  name = "eks-node-group-example"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node-example-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node-example.name
-}
-
-resource "aws_iam_role_policy_attachment" "node-example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node-example.name
-}
-
-resource "aws_iam_role_policy_attachment" "node-example-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node-example.name
-}
 
 
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
 
-
-
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "example"
-  node_role_arn   = aws_iam_role.node-example.arn
-  subnet_ids      = [aws_subnet.example1.id,aws_subnet.example2.id]
-  instance_types  = ["t2.nano"]
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id,aws_subnet.example2.id]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.node-example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.node-example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.node-example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSServicePolicy,
   ]
 }
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
