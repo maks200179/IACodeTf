d65685ced2819f85b130953e6bfba843d1d61cd2
Max <maks200179@mail.ru>
2020-08-07 23:05:50 +0300

    Update aws_eks_cluster.tf

--- fargate-k8s-eks/aws_eks_cluster.tf
+++ fargate-k8s-eks/aws_eks_cluster.tf
@@ -114,7 +114,7 @@ resource "aws_iam_role" "node-example" {
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "eks-fargate-pods.amazonaws.com"
+        Service = "ec2.amazonaws.com"
       }
     }]
     Version = "2012-10-17"
