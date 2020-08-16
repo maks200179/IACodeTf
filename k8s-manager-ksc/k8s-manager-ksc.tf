313c4489a147ef9cb5ed364372eb1c12746173ca
Max <maks200179@mail.ru>
2020-08-16 13:41:08 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -113,37 +113,36 @@ module "vpc" {
   }
 }
   
-  
-module "my-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.16"
-  subnets         = module.vpc.public_subnets
-  vpc_id          = module.vpc.vpc_id
-
-
-  worker_groups = [
+module "eks" {
+  source                        = "terraform-aws-modules/eks/aws"
+  version                       = "12.0.0"
+  cluster_name                  = local.cluster_name
+  subnets                       = module.vpc.public_subnets
+  vpc_id                         = module.vpc.vpc_id
+  worker_groups_launch_template = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
   ]
-
   worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   map_roles                            = var.map_roles
   map_users                            = var.map_users
-  map_accounts                         = var.map_accounts
+  map_accounts                         = var.map_accounts  
 }
+  
+
+
     
     
 
