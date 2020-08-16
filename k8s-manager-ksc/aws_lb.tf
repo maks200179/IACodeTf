f4e8fbd17a31662ef9a77a7fa0e4f382bd06660c
Max <maks200179@mail.ru>
2020-08-16 21:33:30 +0300

    Update and rename aws_lb.tf1 to aws_lb.tf

--- k8s-manager-ksc/aws_lb.tf
+++ k8s-manager-ksc/aws_lb.tf
@@ -21,17 +21,3 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
-module "alb_ingress_controller" {
-  source  = "iplabs/alb-ingress-controller/kubernetes"
-  version = "3.4.0"
-
-  providers = {
-    kubernetes = "kubernetes.eks"
-  }
-
-  k8s_cluster_type = "eks"
-  k8s_namespace    = "kube-system"
-
-  aws_region_name  = data.aws_region.current.name
-  k8s_cluster_name = data.aws_eks_cluster.target.name
-}
