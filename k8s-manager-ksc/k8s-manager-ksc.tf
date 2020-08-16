193aeb5646be206e0d36720dcccd233c08fdc053
Max <maks200179@mail.ru>
2020-08-16 23:01:53 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -149,7 +149,20 @@ module "eks" {
 
     
     
+module "alb_ingress_controller" {
+  source  = "iplabs/alb-ingress-controller/kubernetes"
+  version = "3.4.0"
 
+  providers = {
+    kubernetes = "kubernetes.eks"
+  }
+
+  k8s_cluster_type = "eks"
+  k8s_namespace    = "kube-system"
+
+  aws_region_name  = data.aws_region.current.name
+  k8s_cluster_name = data.aws_eks_cluster.target.name
+}
  
 
   
