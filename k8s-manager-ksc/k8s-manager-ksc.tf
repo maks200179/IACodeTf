12fe088fcd7f199e38d6eaf652ba13f3d5ed8cdd
Max <maks200179@mail.ru>
2020-08-17 01:44:44 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -63,34 +63,58 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 
 
 
-
-
-  
-  
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.my-cluster.cluster_id
 }
 
-
+  
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.my-cluster.cluster_id
 }  
 
 data "aws_availability_zones" "available" {
 }
-
-
-
-
-provider "kubernetes" {
   
+  
+provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.2"
+  version                = "~> 1.9"
 }
 
+  
+##//get vpc kubernetes_test id 
+data "aws_vpc" "kubernetes_test-vpc" {
+  tags = {
+    Name = "dev"
+  }
+}
+
+  
+  
+  
+  
+ 
+##//get subnets ids
+data "aws_subnet_ids" "kubernetes_public-subnet" {
+  vpc_id = "${data.aws_vpc.kubernetes_test-vpc.id}"
+  tags = {
+    Name = "public"
+  }
+}  
+  
+  
+ 
+##//get subnets ids
+data "aws_subnet_ids" "kubernetes_private-subnet" {
+  vpc_id = "${data.aws_vpc.kubernetes_test-vpc.id}"
+  tags = {
+    Name = "private"
+  }
+}  
+    
   
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -115,16 +139,17 @@ module "vpc" {
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
+  
+  
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.16"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-     
 
-module "eks" {
-  source                        = "terraform-aws-modules/eks/aws"
-  version                       = "12.0.0"
-  cluster_name                  = local.cluster_name
-  subnets                       = module.vpc.public_subnets
-  vpc_id                        = module.vpc.vpc_id
-  worker_groups_launch_template = [
+  worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.micro"
@@ -140,14 +165,12 @@ module "eks" {
       asg_desired_capacity          = 1
     },
   ]
+
   worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   map_roles                            = var.map_roles
   map_users                            = var.map_users
-  map_accounts                         = var.map_accounts  
+  map_accounts                         = var.map_accounts
 }
-  
-
-
     
 
 
