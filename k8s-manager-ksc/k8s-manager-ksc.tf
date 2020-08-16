501be7d4d1c3a39849bc700b776e0550bb67c755
Max <maks200179@mail.ru>
2020-08-16 11:59:20 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -145,27 +145,7 @@ module "my-cluster" {
 }
     
     
-    
-data "aws_eks_cluster" "cluster" {
-  name = module.my-cluster.cluster_id
-}
 
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.my-cluster.cluster_id
-}  
-
-
-
-
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-}
-  
   
   
   
