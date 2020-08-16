e9fd24e9aac70099ab7a0b3967f39874adac60d0
Max <maks200179@mail.ru>
2020-08-16 23:17:59 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -83,11 +83,12 @@ data "aws_availability_zones" "available" {
 
 
 provider "kubernetes" {
+  alias                  = "eks"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.12"
+  version                = "~> 1.19"
 }
 
   
