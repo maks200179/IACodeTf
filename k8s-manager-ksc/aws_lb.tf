57ae38800cb23da6202bee8c20da9c0af29a16ff
Max <maks200179@mail.ru>
2020-08-18 00:51:13 +0300

    Update aws_lb.tf

--- k8s-manager-ksc/aws_lb.tf
+++ k8s-manager-ksc/aws_lb.tf
@@ -2,10 +2,9 @@ data "aws_route53_zone" "this" {
   name = local.domain_name
 }
 
-
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
-  version = "~> 2.0"
+  version = "~> v2.0"
 
   domain_name = local.domain_name # trimsuffix(data.aws_route53_zone.this.name, ".") # Terraform >= 0.12.17
   zone_id     = data.aws_route53_zone.this.id
