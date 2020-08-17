6718aa036a096ac5b7fa59ecd4c0ab1fcfd7c637
Max <maks200179@mail.ru>
2020-08-17 22:57:35 +0300

    Update aws_lb.tf

--- k8s-manager-ksc/aws_lb.tf
+++ k8s-manager-ksc/aws_lb.tf
@@ -4,7 +4,7 @@ module "security_group" {
 
   name        = "alb-sg-alb"
   description = "Security group for example usage with ALB"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["http-80-tcp", "all-icmp"]
