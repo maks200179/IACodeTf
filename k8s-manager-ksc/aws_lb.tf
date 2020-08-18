318a9cebcc54b5653daf3068ffdd0b8c937e30f5
Max <maks200179@mail.ru>
2020-08-18 13:15:38 +0300

    Update aws_lb.tf

--- k8s-manager-ksc/aws_lb.tf
+++ k8s-manager-ksc/aws_lb.tf
@@ -46,7 +46,7 @@ module "alb" {
 
   target_groups = [
     {
-      name_prefix      = "pref-"
+      name_prefix      = "pref"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
