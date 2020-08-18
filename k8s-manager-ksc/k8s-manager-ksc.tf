ba8e35f9c9e66ef6b0eeb3552db9bcd60a15f84e
Max <maks200179@mail.ru>
2020-08-18 15:27:42 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -29,7 +29,6 @@ resource "aws_security_group" "all_worker_mgmt" {
   }
 }
 
-
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
@@ -38,23 +37,14 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
+    cidr_blocks = ["10.0.0.0/8"]
   }
-  
-    
-    
-    
+
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 }
 
@@ -70,9 +60,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
     to_port   = 80
     protocol  = "tcp"
 
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
+    cidr_blocks = ["192.168.0.0/16"]
   }
   
     
@@ -81,9 +69,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
     to_port   = 22
     protocol  = "tcp"
 
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
+    cidr_blocks = ["192.168.0.0/16"]
   }
 }
 
