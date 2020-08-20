d13b25d6a20407bbc950df8569b0caea9d7e2e77
Max <maks200179@mail.ru>
2020-08-21 00:01:12 +0300

    Update iam.tf

--- k8s-manager-ksc/iam.tf
+++ k8s-manager-ksc/iam.tf
@@ -1,7 +1,6 @@
-# Setup for IAM role needed to setup an EKS cluster
-resource "aws_iam_role" "tf-eks-master" {
-  name = "terraform-eks-cluster"
- 
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-role"
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -17,10 +16,55 @@ resource "aws_iam_role" "tf-eks-master" {
 }
 POLICY
 }
- 
-resource "aws_iam_role_policy_attachment" "tf-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.tf-eks-master.name}"
-}
- 
 
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+#####
+# EKS Node Group per availability zone
+# If you are running a stateful application across multiple Availability Zones that is backed by Amazon EBS volumes and using the Kubernetes Cluster Autoscaler,
+# you should configure multiple node groups, each scoped to a single Availability Zone. In addition, you should enable the --balance-similar-node-groups feature.
+#
+# In this setup you can configure a single IAM Role that is attached to multiple node groups.
+#####
+
+resource "aws_iam_role" "main" {
+  name = "eks-managed-group-node-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "main_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.main.name
+}
