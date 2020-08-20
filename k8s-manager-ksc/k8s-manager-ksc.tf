40a24917e8b084c08809d54501c15c3ceca5bcfe
Max <maks200179@mail.ru>
2020-08-20 18:21:10 +0300

    Update k8s-manager-ksc.tf

--- k8s-manager-ksc/k8s-manager-ksc.tf
+++ k8s-manager-ksc/k8s-manager-ksc.tf
@@ -250,7 +250,7 @@ module "eks-node-group-a" {
   enabled         = true
   create_iam_role = false
 
-  cluster_name  = module.my-cluster.cluster.id
+  cluster_name  = module.my-cluster.name
   node_role_arn = aws_iam_role.main.arn
   subnet_ids    = [module.vpc.public_subnets]
 
