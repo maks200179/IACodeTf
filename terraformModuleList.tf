467f21d73366044bfdd148f0f903482a2d62805b
Max <maks200179@mail.ru>
2025-01-10 14:10:58 -0500

    Update terraformModuleList.tf

--- terraformModuleList.tf
+++ terraformModuleList.tf
@@ -10,9 +10,9 @@ module "aws_kubernetes_test_network_terraform_conf" {
   source = "../moduls/iacode/docker_compose_env_kubernetes_test_server_kubernetes_worker1"
 }  
 
- module "k8s-manager-ksc" {
-  source = "../moduls/iacode/k8s-manager-ksc"
-} 
+# module "k8s-manager-ksc" {
+#  source = "../moduls/iacode/k8s-manager-ksc"
+#} 
 
  module "fargate-k8s-eks" {
   source = "../moduls/iacode/fargate-k8s-eks"
