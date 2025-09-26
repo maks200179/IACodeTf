0a8215efec99553ecd394d3ac07b8ba8e87d3e97
Max <maks200179@mail.ru>
2025-09-26 15:00:51 -0400

    Update vpc.tf

--- ecs_cluster_terraform1/vpc.tf
+++ ecs_cluster_terraform1/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public[0].id
 
   tags = {
     Name = "ecs-nat-gateway"
