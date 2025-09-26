3f9b3dc318c53e69acd41459e5fe5690de9c1a05
Max <maks200179@mail.ru>
2025-09-26 14:35:14 -0400

    Update security.tf

--- ecs_cluster_terraform1/security.tf
+++ ecs_cluster_terraform1/security.tf
@@ -36,3 +36,14 @@ resource "aws_security_group" "task_sg" {
     cidr_blocks = ["0.0.0.0/0"] 
   }
 }
+
+
+resource "aws_security_group_rule" "ecs_https_egress" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.task_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
