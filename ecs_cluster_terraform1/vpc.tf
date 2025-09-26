64a77ea14bda92c3290780bfc1e8cb7df4fb5755
Max <maks200179@mail.ru>
2025-09-26 14:52:14 -0400

    Update vpc.tf

--- ecs_cluster_terraform1/vpc.tf
+++ ecs_cluster_terraform1/vpc.tf
@@ -8,6 +8,15 @@ resource "aws_internet_gateway" "gw" {
   tags   = { Name = "demo-igw" }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
+  }
+}
+
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
