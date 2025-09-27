75f4f32f9de17a00d51ad2eea726f559feacff72
Max <maks200179@mail.ru>
2025-09-27 11:07:25 -0400

    Update variables.tf

--- ecs_private_alb/variables.tf
+++ ecs_private_alb/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "enable_https" {
   description = "Enable HTTPS listener on ALB. Set true only after you have a valid ACM certificate ARN."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "acm_certificate_arn" {
